2f9da9eac2d348d729d9f0d1c9adf8b27c158152
Kelvin Njeri <knjeri@kinetixsolutions.com>
2020-06-05 02:30:45 -0400

    started adding stocks functions

--- log.py
+++ log.py
@@ -1,20 +1,19 @@
+import os
 import sqlite3
-import os, sys
+import sys
 from datetime import datetime
 
-conn = sqlite3.connect('db.sqlite3')
+conn = sqlite3.connect('trader.db')
 cursor = conn.cursor()
 
 
 def get_sys_info(err_msg):
     exc_type, exc_obj, exc_tb = sys.exc_info()
-    exception = exc_type.__name__
-    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    line = str(exc_tb.tb_lineno)
-    message = str(err_msg)
-    msg = "%s\tFile: %s; line: %s - %s\n" % (exception, fname, line, message)
+    exception, file_name = exc_type.__name__, os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    line, message = str(exc_tb.tb_lineno), str(err_msg)
+    msg = "%s\tFile: %s; line: %s - %s\n" % (exception, file_name, line, message)
+    log_error(exception=exception, file=file_name, line=line, message=message)
     print(msg)
-    log_error(exception=exception, file=fname, line=line, message=message)
 
 
 def setup():
@@ -29,14 +28,30 @@ def setup():
         get_sys_info(setup_err)
 
 
-def log_activity(action=None):
-    cursor.execute('INSERT INTO activity_log VALUES ("%s", "%s")' % (datetime.now(), action))
+def log_activity(action='Testing'):
+    try:
+        cursor.execute('INSERT INTO activity_log VALUES (?,?)', (datetime.now(), action,))
+    except Exception as setup_err:
+        get_sys_info(setup_err)
 
 
 def log_error(**kwargs):
-    cursor.execute(
-        'INSERT INTO error_logs VALUES ("%s", "%s", "%s", "%s", "%s")' % (datetime.now(), kwargs['exception'], kwargs['file'], kwargs['line'], kwargs['message'])
-    )
+    try:
+        cursor.execute(
+            'INSERT INTO error_logs VALUES (?,?,?,?,?)',
+            (datetime.now(), kwargs['exception'], kwargs['file'], kwargs['line'], kwargs['message'],)
+        )
+    except Exception as setup_err:
+        get_sys_info(setup_err)
 
 
-log_activity("run set up ")
+# setup()
+# log_activity()
+# log_activity()
+# log_activity()
+
+cursor.execute('SELECT * FROM activity_log ORDER BY date')
+for row in cursor.fetchall():
+    print(row)
+
+cursor.close()
--- robinhoodapi.py
+++ robinhoodapi.py
@@ -1,12 +1,59 @@
-import robin_stocks as dat_trader
+import robin_stocks as bot_trader
+import sys, os
 from decouple import config, Csv
 
 
 try:
-    sign_in = dat_trader.login(username=config('USERNAME'), password=config('PASSWORD'))
+    sign_in = bot_trader.login(username=config('USERNAME'), password=config('PASSWORD'))
 except Exception as login_err:
     print(str(login_err))
 
 
+def exception_log(err):
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    file_name, exception = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1], exc_type.__name__
+    line, message = str(exc_tb.tb_lineno), str(err)
+    print(f'{exception}\tFile: {file_name} | Line: {line} Message: {message}')
 
 
+def view_holdings():
+    print("\nPresenting all current user holdings")
+    print('---------------------------- --------')
+    try:
+        for ticker, data in bot_trader.build_holdings().items():
+            print(ticker, ': ')
+            for key, value in data.items():
+                print('\t', key, ': ', value)
+    except Exception as holdings_err:
+        exception_log(holdings_err)
+    print('------------------------------------\n')
+
+
+def buy_shares(symbol, amount):
+    print(f'\nBuying {amount} share(s) from {symbol}')
+    print('------------------------------------')
+    try:
+        bot_trader.order_buy_market(symbol=symbol, quantity=amount)
+    except Exception as holdings_err:
+        exception_log(holdings_err)
+    print('------------------------------------\n')
+
+
+def sell_shares(symbol, amount):
+    print(f'\nSelling {amount} share(s) from {symbol}')
+    print('------------------------------------')
+    try:
+        bot_trader.order_sell_market(symbol=symbol, quantity=amount)
+    except Exception as holdings_err:
+        exception_log(holdings_err)
+    print('------------------------------------\n')
+
+
+def buy_crypto(symbol, amount):
+    print(f'\nBuying {symbol} crypto coins')
+    print('------------------------------------')
+    try:
+        bot_trader.order_buy_crypto_by_price(symbol=symbol, amountInDollars=amount)
+    except Exception as holdings_err:
+        exception_log(holdings_err)
+    print('------------------------------------\n')
